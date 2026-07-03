fa0d7c5d25a0fc3bb241cf615ee251cc87aebd1d
victorxixes <victor.tomas.c@gmail.com>
2026-07-03 10:31:38 +0200

    Fix import statement for api in InformesPage

--- frontend/src/pages/InformesPage.tsx
+++ frontend/src/pages/InformesPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { api } from "../utils/api";
+import  api  from "../utils/api";
 
 export const InformesPage = () => {
   const [informes, setInformes] = useState<any[]>([]);

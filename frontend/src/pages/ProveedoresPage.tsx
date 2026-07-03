a579051c73ad2d61e938147262af5004402262a7
victorxixes <victor.tomas.c@gmail.com>
2026-07-03 10:32:51 +0200

    Update ProveedoresPage.tsx

--- frontend/src/pages/ProveedoresPage.tsx
+++ frontend/src/pages/ProveedoresPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { api } from "../utils/api";
+import  api  from "../utils/api";
 
 export const ProveedoresPage = () => {
   const [proveedores, setProveedores] = useState<any[]>([]);

af6ed1a9e3d2a6d60084645ebac6c79365779b60
victorxixes <victor.tomas.c@gmail.com>
2026-07-03 08:37:48 +0200

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:1234@localhost:5432/ERP_FISCAL"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

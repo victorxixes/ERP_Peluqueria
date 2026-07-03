6b9aa809a035345921167471900b0e22a91a66ae
victorxixes <victor.tomas.c@gmail.com>
2026-07-03 10:32:02 +0200

    Update IRPFPage.tsx

--- frontend/src/pages/IRPFPage.tsx
+++ frontend/src/pages/IRPFPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { api } from "../utils/api";
+import  api  from "../utils/api";
 
 export const IRPFPage = () => {
   const [previsiones, setPrevisiones] = useState<any[]>([]);

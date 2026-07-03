c74b9760fd7d65920179ef1ac0625ae1c3943169
victorxixes <victor.tomas.c@gmail.com>
2026-07-03 10:31:24 +0200

    Update LogsPage.tsx

--- frontend/src/pages/LogsPage.tsx
+++ frontend/src/pages/LogsPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { api } from "../utils/api";
+import  api  from "../utils/api";
 
 export const LogsPage = () => {
   const [logs, setLogs] = useState<any[]>([]);

765407979f2189292d572923b9cbbdc03542bdf8
victorxixes <victor.tomas.c@gmail.com>
2026-07-03 10:01:10 +0200

    Update DashboardPage.tsx

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { api } from "../utils/api";
+import  api  from "../utils/api";
 
 export const DashboardPage = () => {
   const [data, setData] = useState<any>(null);

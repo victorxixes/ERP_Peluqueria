9ee4bc3177fab82f3724d1bfafa3a9b3bc96956d
victorxixes <victor.tomas.c@gmail.com>
2026-07-03 10:02:31 +0200

    Update BackupPage.tsx

--- frontend/src/pages/BackupPage.tsx
+++ frontend/src/pages/BackupPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { api } from "../utils/api";
+import  api  from "../utils/api";
 
 export const BackupPage = () => {
   const [backups, setBackups] = useState<any[]>([]);

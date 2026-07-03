a0faa776910c5f0c8ab886fa1f3f6ef995aef017
victorxixes <victor.tomas.c@gmail.com>
2026-07-03 10:32:45 +0200

    Update UsuariosPage.tsx

--- frontend/src/pages/UsuariosPage.tsx
+++ frontend/src/pages/UsuariosPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { api } from "../utils/api";
+import  api  from "../utils/api";;
 
 export const UsuariosPage = () => {
   const [usuarios, setUsuarios] = useState<any[]>([]);

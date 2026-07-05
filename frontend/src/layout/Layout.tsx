1a38f6c7845f98d7f627bc6189071b99ad367193
victorxixes <victor.tomas.c@gmail.com>
2026-07-05 17:00:23 +0200

    Fix layout div structure for consistency

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -2,17 +2,22 @@ import { Sidebar } from "../components/Sidebar";
 
 export const Layout = ({ children }: { children: any }) => {
   return (
-    <div className="flex bg-slate-900 text-slate-200 min-h-screen">
+    <div className="min-h-screen flex bg-slate-900 text-slate-200">
+
+      {/* Sidebar */}
       <Sidebar />
 
+      {/* Contenido */}
       <div className="flex-1 flex flex-col">
         <main className="flex-1 p-8 overflow-y-auto">
           {children}
         </main>
       </div>
+
     </div>
   );
 };
+
 
 
 

cf9f989803d4ef685aead1c18435c23d9e6a17d4
victorxixes <victor.tomas.c@gmail.com>
2026-07-05 17:28:07 +0200

    Update Layout.tsx

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -4,15 +4,19 @@ import { Outlet } from "react-router-dom";
 export const Layout = () => {
   return (
     <div className="min-h-screen flex bg-slate-900 text-slate-200">
+
       <Sidebar />
+
       <div className="flex-1 flex flex-col">
         <main className="flex-1 p-8 overflow-y-auto">
           <Outlet />
         </main>
       </div>
+
     </div>
   );
 };
+
 
 
 

8b62fdb87ebd4d450ce2d7b7052f864fd3fd5fb6
victorxixes <victor.tomas.c@gmail.com>
2026-07-06 13:29:41 +0200

    Update ActivosPage.tsx

--- frontend/src/pages/ActivosPage.tsx
+++ frontend/src/pages/ActivosPage.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useState } from "react";
 import api from "../utils/api";
 
 export const ActivosPage = () => {

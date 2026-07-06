504d7c5f4a6a1ce5401b7f8ec429c5d7cb89e88f
victorxixes <victor.tomas.c@gmail.com>
2026-07-06 08:52:26 +0200

    Update Layout.tsx

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -1,20 +1,18 @@
 import { Sidebar } from "../components/Sidebar";
 import { Outlet } from "react-router-dom";
-import import "../styles/Layout.css";
+import "../styles/Layout.css";
 
 export const Layout = () => {
   return (
     <div className="layout-container">
-
       <Sidebar />
-
       <div className="layout-content">
         <Outlet />
       </div>
-
     </div>
   );
 };
+
 
 
 

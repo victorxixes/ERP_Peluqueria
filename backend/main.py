138f8fe4e61f070698fc53076f84a5ebf1b4d9e7
victorxixes <victor.tomas.c@gmail.com>
2026-07-06 20:16:40 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -63,7 +63,11 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://erp-peluqueria-front.onrender.com"],
+    allow_origins=[
+        "https://erp-peluqueria-front.onrender.com",
+        "https://erp-peluqueria-front.onrender.com/",
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

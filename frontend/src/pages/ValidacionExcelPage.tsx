db6513c07377867aec43ce8da3e988d7f01329ab
victorxixes <victor.tomas.c@gmail.com>
2026-07-06 13:33:37 +0200

    Update ValidacionExcelPage.tsx

--- frontend/src/pages/ValidacionExcelPage.tsx
+++ frontend/src/pages/ValidacionExcelPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const ValidacionExcelPage = () => {
   const [tipo, setTipo] = useState<"ingresos" | "gastos">("ingresos");

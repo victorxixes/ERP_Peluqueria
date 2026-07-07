f8f919e454d2cc85361d4da5390b9b2e35ad6872
victorxixes <victor.tomas.c@gmail.com>
2026-07-07 13:01:49 +0200

    Update irpf.py

--- backend/routers/irpf.py
+++ backend/routers/irpf.py
@@ -7,8 +7,10 @@ from services.irpf_service import calcular_irpf_anual
 router = APIRouter(prefix="/irpf", tags=["IRPF"])
 
 @router.get("/anual")
-def irpf_anual(
-    year: int,
-    db: Session = Depends(get_db)
-):
-    return calcular_irpf_anual(db, year)
+def irpf_anual(year: int, db: Session = Depends(get_db)):
+    try:
+        return calcular_irpf_anual(db, year)
+    except Exception as e:
+        print("ERROR IRPF:", e)
+        raise e
+

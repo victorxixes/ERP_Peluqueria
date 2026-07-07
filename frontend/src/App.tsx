7b0ba33ae8c9a2b03dab70e65cd9bc463c1c3af5
victorxixes <victor.tomas.c@gmail.com>
2026-07-07 08:14:13 +0200

    Import ProtectedRoute component in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./layout/Layout";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 import { LoginPage } from "./pages/LoginPage";
 
